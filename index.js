a8fcce6bb3fc7635abb6e70f9908c68bc1b66a82
shy0215 <66727782+shy0215@users.noreply.github.com>
2023-01-05 13:19:27 +0900

    mongoose

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://shy_0215:12345@login-temp.g5pfoy1.mongodb.net/?retryWrites=true&w=majority'
+).then(()=> console.log('MongoDB connected...'))
+ .catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
