3c3d1aafd70f93a592e994feffae436eeec50105
shy0215 <66727782+shy0215@users.noreply.github.com>
2023-01-09 14:12:18 +0900

    token 생성(+localhost 에러)

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
 const {User} = require("./models/User");
-
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+
 mongoose.connect(config.mongoURI
 ).then(()=> console.log('MongoDB connected...'))
  .catch(err => console.log(err))
@@ -17,6 +17,7 @@ mongoose.connect(config.mongoURI
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -34,6 +35,29 @@ app.post('/register', (req, res)=>{
   })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//로그인 라우트 만들기
+app.post('/login', (req,res) => {
+  //요청된 이메일을 DB에서 있는지 찾기
+  User.findOne({email: req.body.email}, (err, user)=>{ 
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message:"존재하지 않는 이메일 입니다."
+      })
+    }
+    //요청된 이메일이 있으면 PW가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+      return res.json({loginSuccess: false, message:"비밀번호가 틀렸습니다."})
+      //PW가 맞다면 token 생성
+      user.generateToken((err, user)=>{
+        if(err) return res.status(400).send(err);
+        //토큰저장 (쿠키, 로컬스토리지 등등..에 가능)
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess:true, userId: user._id})
+
+      })
+    })
+  })
 })
